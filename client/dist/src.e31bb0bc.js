aa0f62de2cddd982aef0fd49dc2bbf0c87ee33e0
ygeszvain <34775549+ygeszvain@users.noreply.github.com>
2020-09-18 16:32:47 -0500

    bug fix

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -49458,7 +49458,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51450" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63154" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
